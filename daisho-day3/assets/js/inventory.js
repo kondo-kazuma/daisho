f139e2d4f9b647df923bc90f6f3bc949cf256ab0
kondo-kazuma <jyizhen919@gmail.com>
2025-11-20 14:45:28 +0900

    2025 1120 v6

--- daisho-day3/assets/js/inventory.js
+++ daisho-day3/assets/js/inventory.js
@@ -5,7 +5,7 @@
 const items = [
     { id:"D001", name:"オレンジジュース", price: 150, stock: 20, minStock: 5},
     { id:"D002", name:"コーラ",          price: 150, stock: 12, minStock: 5 },
-    { id:"D003", name: "お茶",           price: 120, stock: 30, minStock: 10 }
+    { id:"D003", name:"お茶",           price: 120, stock: 30, minStock: 10 }
 ];
 
 //HTML で <tbody id="item-list"> と書かれた部分を JavaScript から取得する
@@ -15,7 +15,7 @@ const tbody = document.getElementByld("item-list");
 //2.表示用の関数 render()
 // ---------------------
 function render(){
-    // 一度、中身を殻にしてから作り直す
+    // 一度、中身を空にしてから作り直す
     tbody.innerHTML = "";
 
     // items の中身を １件ずつ取り出して tr（行）を作る
