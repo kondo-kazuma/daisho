ef8160932253de5987b86c2bfd8fed288a40b877
kondo-kazuma <jyizhen919@gmail.com>
2025-11-20 14:42:15 +0900

    2025 1120 v5

--- daisho-day3/assets/js/inventory.js
+++ daisho-day3/assets/js/inventory.js
@@ -1,3 +1,7 @@
+// ----------------------
+//1.在庫データ（ JSON 的な構造）
+// ----------------------
+
 const items = [
     { id:"D001", name:"オレンジジュース", price: 150, stock: 20, minStock: 5},
     { id:"D002", name:"コーラ",          price: 150, stock: 12, minStock: 5 },
